cc99ae32622a279a2e67144df2efef6898ab090b
Bborde1 <Brandon.Borde@gmail.com>
2025-07-30 17:17:45 -0400

    Added FundMe and PriceConverter contracts as well as wrote tests for values in FundMe

---
+++ src/FundMe.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// Get funds from users
+// Withdraw funds
+//
+
+// import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+import {PriceConverter} from "./PriceConverter.sol";
+
+error FundMe__NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded)
+        public addressToAmountFunded;
+
+    // constants are set once outside of a function
+    uint256 public constant MINIMUM_USD = 5e18;
+    // immutables are set once in a function, or set once outside of their declaration
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        // 1. How do we send ETH to this contract
+        require(
+            msg.value.getConversionRate() >= MINIMUM_USD,
+            "Not enough ETH sent"
+        );
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public only_Owner {
+        require(msg.sender == i_owner, "Must be i_owner to withdraw");
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // Send with transfer
+        payable(msg.sender).transfer(address(this).balance);
+        // Send with send
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send Failed");
+        // Send with call
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier only_Owner() {
+        // require(msg.sender == i_owner, "Senter is not i_owner");
+        if (msg.sender != i_owner) {
+            revert FundMe__NotOwner();
+        }
+        _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+}
